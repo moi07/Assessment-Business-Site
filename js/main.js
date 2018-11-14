93f4ac5e951b3e27aaf27000c1c995f866069fb7
moi07 <mjordash@gmail.com>
2018-11-14 16:24:01 +0200

    copyright year optimized

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 let menu = document.querySelector('label.menu');
 //menu.addEventListener('click', alert('what!'),false);
+const copyYear = document.querySelector('.copyrightYear');
+var today=new Date();
+var y = today.getFullYear();
+copyYear.textContent = y;
